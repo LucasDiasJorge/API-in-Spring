b28ee93de9e066daab69757cf3d4abc02447d4cd
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-12-14 10:42:37 -0300

    Adding JVM Timezone as Amercia/Sao_Paulo

--- src/main/java/com/project/core/Main.java
+++ src/main/java/com/project/core/Main.java
@@ -1,5 +1,7 @@
 package com.project.core;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
 	public static void main(String[] args) {
+
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+
 		System.out.println(" CORE is running ... ");
 		SpringApplication.run(Main.class, args);
 	}
