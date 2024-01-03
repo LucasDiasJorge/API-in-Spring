3e27cfc680018f5596cd8412cfb815a3d34929c5
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-01-03 11:20:05 -0300

    Update UserRoleService.java

--- src/main/java/com/project/core/service/UserRoleService.java
+++ src/main/java/com/project/core/service/UserRoleService.java
@@ -1,12 +1,8 @@
 package com.project.core.service;
 
 import java.security.Principal;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -21,8 +17,8 @@ import com.project.core.repository.UserRepository;
 @Service
 public class UserRoleService extends AbstractService<UserModel> {
 
-    private UserRepository userRepository;
-    private RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     protected UserRoleService(UserRepository userRepository, RoleRepository roleRepository) {
         super(userRepository);
@@ -30,100 +26,96 @@ public class UserRoleService extends AbstractService<UserModel> {
         this.roleRepository = roleRepository;
     }
 
-    public UserModel switchRole(Long userId, String roleName, Principal principal) throws AppException {
-        if (roleName.equals("root") || roleName.equals("ROOT")) {
-            // Not authorized to set a user as root;
-            return null;
+    public UserModel switchRole(Map<String, Object> body, Principal principal) throws AppException {
+        RoleModel role = getRoleFromId((Number) body.get("roleId"));
+
+        if (role.getRoleName() == RoleName.ROLE_ROOT) {
+            throw new AppException("No permission to set a user as a Root", 418, null);
         }
 
-        UserModel user = userRepository.findByEmail(principal.getName());
+        UserModel user = getUserFromPrincipal(principal);
         CompanyModel company = user.getCompany();
-        Optional<UserModel> userFind = userRepository.findById(userId);
+        UserModel userModify = getUserById((Number) body.get("userId"));
 
-        if (!userFind.isPresent()) {
-            // Execption, user not found;
-            return null;
-        }
+        checkPermissionAndModifyRole(user, company, userModify, role, principal);
 
-        UserModel userModify = userFind.get();
-
-        RoleModel role = roleRepository.findRole(RoleName.valueOf("ROLE_" + roleName.toUpperCase())).orElseThrow();
-
-        if (company.isHeadquarters()) {
-            if (isBranchInHQ(company, userFind.get().getCompany().getId())
-                    || userModify.getCompany().getId() == company.getId()) {
-
-                List<RoleModel> roles = new ArrayList<>();
-                roles.add(role);
-                userModify.getRoles().clear();
-                userModify.setRoles(roles);
-                userModify.setUpdatedBy(user.getId());
-                userModify.setLastModifiedDate(new Date());
-                return update(userId, userModify, userFind.get(), principal);
-
-            }
-        } else {
-            if (company.getId() == userModify.getCompany().getId()) {
-                List<RoleModel> roles = new ArrayList<>();
-                roles.add(role);
-                userModify.getRoles().clear();
-                userModify.setRoles(roles);
-                userModify.setUpdatedBy(user.getId());
-                userModify.setLastModifiedDate(new Date());
-                return update(userId, userModify, userFind.get(), principal);
-
-            } else {
-                // Exception, trying to edit user frm another company;
-                return null;
-            }
-        }
-
-        return null;
+        return updateUserInfo(userModify, principal);
     }
 
-    public UserModel switchRoleRoot(Long userId, String roleName, Principal principal) throws AppException {
-        UserModel user = userRepository.findByEmail(principal.getName());
-        Optional<UserModel> userFind = userRepository.findById(userId);
+    public List<UserModel> switchRoles(List<Map<String, Object>> bodies, Principal principal) throws AppException {
+        List<UserModel> updatedUsers = new ArrayList<>();
 
-        if (!userFind.isPresent()) {
-            // Execption, user not found;
-            return null;
+        for (Map<String, Object> body : bodies) {
+            RoleModel role = getRoleFromId((Number) body.get("roleId"));
+
+            if (role.getRoleName() == RoleName.ROLE_ROOT) {
+                throw new AppException("No permission to set a user as a Root", 418, null);
+            }
+
+            UserModel user = getUserFromPrincipal(principal);
+            CompanyModel company = user.getCompany();
+            UserModel userModify = getUserById((Number) body.get("userId"));
+
+            checkPermissionAndModifyRole(user, company, userModify, role, principal);
+
+            updatedUsers.add(updateUserInfo(userModify, principal));
         }
 
-        UserModel userModify = userFind.get();
-
-        RoleModel role = roleRepository.findRole(RoleName.valueOf("ROLE_" + roleName.toUpperCase())).orElseThrow();
-        List<RoleModel> roles = new ArrayList<>();
-        roles.add(role);
-        userModify.getRoles().clear();
-        userModify.setRoles(roles);
-        userModify.setUpdatedBy(user.getId());
-        userModify.setLastModifiedDate(new Date());
-        return update(userId, userModify, userFind.get(), principal);
+        return updatedUsers;
     }
+
 
     public Object getUserInRoles(Principal principal) {
-        CompanyModel companyModel = userRepository.findByEmail(principal.getName()).getCompany();
-        List<Map<String, Object>> l = new ArrayList<>();
-        for (RoleName e : RoleName.values()) {
-            if (e.toString() != "ROLE_PORTAL") {
-                Map<String, Object> m = new LinkedHashMap<>();
-                m.put("roleName", e.toString().replace("ROLE_", ""));
-                m.put("users", userRepository.findByRole(e.toString(), companyModel.getId()).get());
-                l.add(m);
-            }
-        }
-        return l;
+        List<RoleName> excludeRolesToShow = List.of(RoleName.ROLE_PORTAL,RoleName.ROLE_ROOT);
+        UserModel user = getUserFromPrincipal(principal);
+        CompanyModel companyModel = user.getCompany();
+        List<RoleModel> roles = roleRepository.findAllRolesExcluding(excludeRolesToShow);
+
+        return roles.stream()
+                .map(role -> createUserRoleMap(role, companyModel))
+                .collect(Collectors.toList());
     }
 
     public boolean isBranchInHQ(CompanyModel hq, Long branchId) {
-        if (hq.getBranchs().stream().anyMatch(e -> e.getId() == branchId)) {
-            return true;
-        } else if (hq.getId() == branchId) {
-            return true;
-        }
-
-        return false;
+        return hq.getBranchs().stream().anyMatch(e -> e.getId() == branchId) || hq.getId() == branchId;
     }
 
+    private RoleModel getRoleFromId(Number roleId) throws AppException {
+        return roleRepository.findById(roleId.longValue())
+                .orElseThrow(() -> new AppException("Role not found", 404, null));
+    }
+
+    private UserModel getUserById(Number userId) throws AppException {
+        return userRepository.findById(userId.longValue())
+                .orElseThrow(() -> new AppException("User not found", 404, null));
+    }
+
+    private void checkPermissionAndModifyRole(UserModel user, CompanyModel company, UserModel userModify, RoleModel role, Principal principal) throws AppException {
+        if (company.isHeadquarters() && (isBranchInHQ(company, userModify.getCompany().getId()) || userModify.getCompany().getId() == company.getId())) {
+            updateUserRoles(userModify, Set.of(role), user, principal);
+        } else if (!company.isHeadquarters() && company.getId() == userModify.getCompany().getId()) {
+            updateUserRoles(userModify, Set.of(role), user, principal);
+        } else {
+            throw new AppException("No permission to change user from another company", 422, null);
+        }
+    }
+
+    private void updateUserRoles(UserModel user, Set<RoleModel> roles, UserModel updater, Principal principal) throws AppException {
+        user.setRoles(new HashSet<>(roles));
+        user.setUpdatedBy(updater.getId());
+        user.setLastModifiedDate(new Date());
+        update(user.getId(), user, user, principal);
+    }
+
+    private Map<String, Object> createUserRoleMap(RoleModel role, CompanyModel companyModel) {
+        Map<String, Object> userRoleMap = new LinkedHashMap<>();
+        userRoleMap.put("id", role.getId());
+        userRoleMap.put("name", role.getRoleName().getName());
+        userRoleMap.put("users", userRepository.findByRole(role.getRoleName().toString(), companyModel.getId()).get());
+        return userRoleMap;
+    }
+
+    private UserModel updateUserInfo(UserModel user, Principal principal) throws AppException {
+        return update(user.getId(), user, user, principal);
+    }
 }
