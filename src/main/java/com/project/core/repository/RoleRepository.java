40a4db84481bb78ca43754bf1f1986b0cb05512f
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-01-03 11:19:47 -0300

    Update RoleRepository.java

--- src/main/java/com/project/core/repository/RoleRepository.java
+++ src/main/java/com/project/core/repository/RoleRepository.java
@@ -1,5 +1,6 @@
 package com.project.core.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,5 +14,11 @@ public interface RoleRepository extends JpaRepository<RoleModel, Long> {
 
     @Query("SELECT r FROM RoleModel r WHERE r.roleName = :roleName ")
     Optional<RoleModel> findRole(@Param("roleName") RoleName roleName);
+
+    @Query("SELECT r FROM RoleModel r ORDER BY r.priority ASC")
+    List<RoleModel> findAllRoles();
+
+    @Query("SELECT r FROM RoleModel r WHERE r.roleName NOT IN :excludes ORDER BY r.priority ASC")
+    List<RoleModel> findAllRolesExcluding(@Param("excludes") List<RoleName> excludes);
 
 }
