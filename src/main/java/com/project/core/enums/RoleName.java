d40898af8da56701d653b3a518fdb94977d41927
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-01-03 11:19:32 -0300

    Update RoleName.java

--- src/main/java/com/project/core/enums/RoleName.java
+++ src/main/java/com/project/core/enums/RoleName.java
@@ -1,9 +1,28 @@
 package com.project.core.enums;
 
 public enum RoleName {
-    ROLE_ROOT,
-    ROLE_ADMIN,
-    ROLE_USER,
-    ROLE_MANAGER,
-    ROLE_PORTAL;
+
+    ROLE_ROOT("Root",0),
+    ROLE_ADMIN("Administrator",1),
+    ROLE_MANAGER("Manager",2),
+    ROLE_USER("User",3),
+    ROLE_MAINTENER("Maintener",4),
+    ROLE_AUDITOR("Auditor",5),
+    ROLE_PORTAL("Portal",6);
+
+    private String name;
+    private Integer priority;
+
+    private RoleName(String name, Integer priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
 }
